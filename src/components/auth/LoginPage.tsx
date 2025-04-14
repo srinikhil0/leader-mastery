57177dc3885b43c290fe232011fee1ad8fd6f4a6
Sri Nikhil Reddy <srinikhilreddy99@gmail.com>
2025-04-14 19:08:31 -0400

    Auth page, social logins update
    Replaced the facebook and instagram social logins with microsoft and apple

--- src/components/auth/LoginPage.tsx
+++ src/components/auth/LoginPage.tsx
@@ -164,34 +164,28 @@ export const LoginPage = () => {
               </svg>
             </button>
 
-            {/* Facebook */}
-            <button className="p-3 border-2 border-gray-200 dark:border-gray-600 rounded-lg 
-              hover:border-primary dark:hover:border-primary 
-              transform transition-all duration-300 hover:scale-105 
-              hover:shadow-md dark:hover:shadow-[0_4px_6px_-1px_rgba(255,255,255,0.1)]
-              focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 dark:ring-offset-gray-800">
-              <svg className="w-6 h-6 mx-auto" fill="#1877F2" viewBox="0 0 24 24">
-                <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
-              </svg>
-            </button>
-
-            {/* Instagram */}
+            {/* Microsoft */}
             <button className="p-3 border-2 border-gray-200 dark:border-gray-600 rounded-lg 
               hover:border-primary dark:hover:border-primary 
               transform transition-all duration-300 hover:scale-105 
               hover:shadow-md dark:hover:shadow-[0_4px_6px_-1px_rgba(255,255,255,0.1)]
               focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 dark:ring-offset-gray-800">
               <svg className="w-6 h-6 mx-auto" viewBox="0 0 24 24">
-                <defs>
-                  <radialGradient id="instagram-gradient" r="150%" cx="30%" cy="107%">
-                    <stop stopColor="#fdf497" offset="0%" />
-                    <stop stopColor="#fdf497" offset="5%" />
-                    <stop stopColor="#fd5949" offset="45%" />
-                    <stop stopColor="#d6249f" offset="60%" />
-                    <stop stopColor="#285AEB" offset="90%" />
-                  </radialGradient>
-                </defs>
-                <path fill="url(#instagram-gradient)" d="M12 0C8.74 0 8.333.015 7.053.072 5.775.132 4.905.333 4.14.63c-.789.306-1.459.717-2.126 1.384S.935 3.35.63 4.14C.333 4.905.131 5.775.072 7.053.012 8.333 0 8.74 0 12s.015 3.667.072 4.947c.06 1.277.261 2.148.558 2.913.306.788.717 1.459 1.384 2.126.667.666 1.336 1.079 2.126 1.384.766.296 1.636.499 2.913.558C8.333 23.988 8.74 24 12 24s3.667-.015 4.947-.072c1.277-.06 2.148-.262 2.913-.558.788-.306 1.459-.718 2.126-1.384.666-.667 1.079-1.335 1.384-2.126.296-.765.499-1.636.558-2.913.06-1.28.072-1.687.072-4.947s-.015-3.667-.072-4.947c-.06-1.277-.262-2.149-.558-2.913-.306-.789-.718-1.459-1.384-2.126C21.319 1.347 20.651.935 19.86.63c-.765-.297-1.636-.499-2.913-.558C15.667.012 15.26 0 12 0zm0 2.16c3.203 0 3.585.016 4.85.071 1.17.055 1.805.249 2.227.415.562.217.96.477 1.382.896.419.42.679.819.896 1.381.164.422.36 1.057.413 2.227.057 1.266.07 1.646.07 4.85s-.015 3.585-.074 4.85c-.061 1.17-.256 1.805-.421 2.227-.224.562-.479.96-.899 1.382-.419.419-.824.679-1.38.896-.42.164-1.065.36-2.235.413-1.274.057-1.649.07-4.859.07-3.211 0-3.586-.015-4.859-.074-1.171-.061-1.816-.256-2.236-.421-.569-.224-.96-.479-1.379-.899-.421-.419-.69-.824-.9-1.38-.165-.42-.359-1.065-.42-2.235-.045-1.26-.061-1.649-.061-4.844 0-3.196.016-3.586.061-4.861.061-1.17.255-1.814.42-2.234.21-.57.479-.96.9-1.381.419-.419.81-.689 1.379-.898.42-.166 1.051-.361 2.221-.421 1.275-.045 1.65-.06 4.859-.06l.045.03zm0 3.678c-3.405 0-6.162 2.76-6.162 6.162 0 3.405 2.76 6.162 6.162 6.162 3.405 0 6.162-2.76 6.162-6.162 0-3.405-2.76-6.162-6.162-6.162zM12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm7.846-10.405c0 .795-.646 1.44-1.44 1.44-.795 0-1.44-.646-1.44-1.44 0-.794.646-1.439 1.44-1.439.793-.001 1.44.645 1.44 1.439z"/>
+                <path fill="#F25022" d="M11.4 24H0V12.6h11.4V24z"/>
+                <path fill="#00A4EF" d="M24 24H12.6V12.6H24V24z"/>
+                <path fill="#7FBA00" d="M11.4 11.4H0V0h11.4v11.4z"/>
+                <path fill="#FFB900" d="M24 11.4H12.6V0H24v11.4z"/>
+              </svg>
+            </button>
+
+            {/* Apple */}
+            <button className="p-3 border-2 border-gray-200 dark:border-gray-600 rounded-lg 
+              hover:border-primary dark:hover:border-primary 
+              transform transition-all duration-300 hover:scale-105 
+              hover:shadow-md dark:hover:shadow-[0_4px_6px_-1px_rgba(255,255,255,0.1)]
+              focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 dark:ring-offset-gray-800">
+              <svg className="w-6 h-6 mx-auto" viewBox="0 0 24 24">
+                <path fill="#000000" d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z"/>
               </svg>
             </button>
 
